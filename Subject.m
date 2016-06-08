65c855edb1079ced6c7f58f7f481cf58b276a851
onatselim <onatselim@gmail.com>
2016-06-08 19:55:24 +0200

    methods can return the same matrix as SPM.xX.X without all the schnickschnack

--- Subject.m
+++ Subject.m
@@ -372,6 +372,48 @@ classdef Subject < Project
             movefile(target_file,self.path_native_atlas);
         end
         
+        function DesignMatrix(self,nrun,model_num)
+
+            keyboard
+            cond = [];
+            load(sprintf('%sdesign/model%02d/data.mat',self.path2data(nrun),model_num))
+            fMRI_T                = 16;
+            fMRI_T0               = 1;
+            xBF.T                 = fMRI_T;
+            xBF.T0                = fMRI_T0;
+            xBF.dt                = self.TR/xBF.T;
+            xBF.UNITS             = 'scans';
+            xBF.Volterra          = 1;
+            xBF.name              = 'hrf';
+            xBF                   = spm_get_bf(xBF);            
+            %%
+            for i = 1:length(cond);%one regressor for each condition
+                Sess.U(i).dt        = xBF.dt;%- time bin (seconds)
+                
+                Sess.U(i).ons       = cond(i).onset;%- onsets    (in SPM.xBF.UNITS)
+                Sess.U(i).name      = {sprintf('%02d',i)};%- cell of names for each input or cause
+                
+                %no parametric modulation here
+                Sess.U(i).dur    =  repmat(0,length(Sess.U(i).ons),1);%- durations (in SPM.xBF.UNITS)
+                Sess.U(i).P.name =  'none';
+                Sess.U(i).P.P    =  'none';
+                Sess.U(i).P.h    =  0;%- order of polynomial expansion
+                Sess.U(i).P.i    =  1;%- sub-indices of u pertaining to P
+            end
+            %
+            k                       = self.total_volumes(nrun);
+            SPM.xBF                 = xBF;
+            SPM.nscan               = k;
+            SPM.Sess                = Sess;
+            SPM.Sess.U              = spm_get_ons(SPM,1);
+            
+            %%
+            % Convolve stimulus functions with basis functions
+            [X,Xn,Fc]               = spm_Volterra(SPM.Sess.U,SPM.xBF.bf,SPM.xBF.Volterra);
+            % Resample regressors at acquisition times (32 bin offset)
+            X                       = X((0:(k - 1))*fMRI_T + fMRI_T0 + 32,:);
+        end
+        
     end
     methods %fmri path_tools which are related to the subject              
         function out        = skullstrip(self,varargin)
